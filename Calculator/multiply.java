52e2e24caafbc68004a5f5a1356e0b39bfd8cf6b
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-12 14:52:49 +0530

    This is my Calculator File

---
+++ Calculator/multiply.java
@@ -0,0 +1,15 @@
+public class multiply{
+    public int add(int jum1,num2){
+        int num=num1*num2;
+        System.out.println(num);
+        return num;
+    }
+    public static void main(String args[]){
+        multiply obj=new multiply();
+        int c=obj.multiply(20,30);
+        int d=obj.add(40,50);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+    
