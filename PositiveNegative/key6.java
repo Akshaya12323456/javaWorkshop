0f4832ccc48b84a760d1fd1bf50c8c26dd846934
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 11:46:29 +0530

    The positive number found

--- PositiveNegative/key6.java
+++ PositiveNegative/key6.java
@@ -1,12 +1,15 @@
 public class key6{
     public static void main(String args[]){
         int num=-12;
-        if(num>0)
-        {
+        if(num>0) {
             System.out.println("The Number is Positive");
         }
-        else
+        else if(num==0)
         {
+            System.out.println("The number is Neutral");
+        }
+                else
+            {
             System.out.println("The Number is Negative");
         }
     }
