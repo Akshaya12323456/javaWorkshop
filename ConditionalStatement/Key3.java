78636739beb82f88ef73bf168ad0dcf42fcf7614
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 10:49:55 +0530

    My ConditionalStatement

---
+++ ConditionalStatement/Key3.java
@@ -0,0 +1,10 @@
+public class key3{
+    public static void main(String args[]){
+        if(true){
+        System.out.println("Statement is true");
+    }
+    if(false){
+    System.out.println("Statement is false");
+    }
+}
+}
