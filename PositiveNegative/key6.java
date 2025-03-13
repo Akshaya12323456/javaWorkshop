dc6fa1b4c75273439752d1c3a4e96fa74675f8ea
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 11:14:09 +0530

    The Number was found

---
+++ PositiveNegative/key6.java
@@ -0,0 +1,13 @@
+public class key6{
+    public static void main(String args[]){
+        int num=-12;
+        if(num>0)
+        {
+            System.out.println("The Number is Positive");
+        }
+        else
+        {
+            System.out.println("The Number is Negative");
+        }
+    }
+}
