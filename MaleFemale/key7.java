3095bcbcbbdd6f37a3fa94f781cafe68b038b365
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 11:32:17 +0530

    This is to find Gender

---
+++ MaleFemale/key7.java
@@ -0,0 +1,13 @@
+public class key7{
+    public static void main(String args[]){
+        String gender="M";
+        if(gender=="M")
+        {
+            System.out.println("The person is Male");
+        }
+        else
+        {
+            System.out.println("The person is female");
+        }
+    }
+}
