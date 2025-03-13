c3634713139bca985da647f9e4e0b2be67afc1f5
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 11:00:45 +0530

    This is my statement

---
+++ Ifelse/key4.java
@@ -0,0 +1,11 @@
+public class key4{
+    public static void main(String args[]){
+        if(false){
+            System.out.println("Statement is true");
+        }
+        else
+        {
+            System.out.println("Statement is false");
+        }
+    }
+}
