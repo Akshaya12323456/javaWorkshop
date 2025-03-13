03ffcaf83213fc3fdbff42302192e6941075a87d
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 10:31:25 +0530

    My conditionalOerator program

---
+++ ConditionalOperator/Demo2.java
@@ -0,0 +1,9 @@
+public class Demo2{
+    public static void main(String args[]){
+        System.out.println(3<9);
+        System.out.println(12>6);
+        System.out.println(9==3);
+        System.out.println(16<2);
+        System.out.println(15==15);
+    }
+}
