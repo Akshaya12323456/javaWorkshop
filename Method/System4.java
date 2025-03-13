f67e182356634474a8e17496f176ad6eeea8fcd5
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 09:40:16 +0530

    My project in Java

---
+++ Method/System4.java
@@ -0,0 +1,10 @@
+public class System4{
+    public void add(int num1,int num2){
+        int num=num1+num2;
+        System.out.println(num);
+    }
+    public static void main(String args[]){
+        System4 obj=new System4();
+        obj.add(10,30);
+    }
+}
