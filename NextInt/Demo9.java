031d74d5e95bdd3a5cb230997db0fbe29194d58f
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 15:11:59 +0530

    The java Program executed

---
+++ NextInt/Demo9.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Demo9{
+    public static void main(String args[]){
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter your Age");
+    int Age=input.nextInt();
+    System.out.println(Age);
+}
+}
