873b01b8d42d9005d5919a42c3ab05d62a4ccaf3
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 11:09:12 +0530

    The problem was solved

---
+++ MajorMinorFinder/key5.java
@@ -0,0 +1,13 @@
+public class key5{
+    public static void main(String args[]){
+        int age=12;
+        if(age>=18)
+        {
+            System.out.println("The person is Major");
+        }
+        else
+        {
+            System.out.println("The person is Minor");
+        }
+    }
+}
