3289fd650a3ff6b01bd00f78162cc91f17f18efe
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-13 15:08:12 +0530

    This is my Works

---
+++ LeapYear/Year.java
@@ -0,0 +1,14 @@
+public class Year{
+    public static void main(String args[]){
+        int year=2020;
+        if(year%4==0)
+        {
+            System.out.println("The year is Leap Year");
+        }
+        else
+        {
+            System.out.println("The year is not Leap Year");
+        }
+        }
+    }
+ 
