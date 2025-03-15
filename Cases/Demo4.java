48e65b165862ef93fd11847b441f7d3f03935d08
@Akshaya12323456 <ayahska752006@gmail.com>
2025-03-15 10:09:42 +0530

    My cases Program

---
+++ Cases/Demo4.java
@@ -0,0 +1,8 @@
+public class Demo4{
+    public static void main(String args[]){
+        String X="Hello Programmer";
+        System.out.println(X.toUpperCase());
+        System.out.println(X.toLowerCase());
+    }
+}
+    
